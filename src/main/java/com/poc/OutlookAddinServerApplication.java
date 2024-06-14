0e0a014ec54ab5c0d2c22aa1f4d2aa65f77cfa0a
BERLIN <BERLIN.LIANG@OOCL.COM>
2024-06-14 11:36:18 +0800

    Initial project

---
+++ src/main/java/com/poc/OutlookAddinServerApplication.java
@@ -0,0 +1,13 @@
+package com.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutlookAddinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutlookAddinServerApplication.class, args);
+	}
+
+}
